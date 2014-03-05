ffc9da25122ddc57093440cc040be1989d6ad5a6
Adam Malantonio <adam.malantonio@gmail.com>
2014-03-05 14:25:23 -0500

    refactored / search on selection
    0.2 update now pulls selected text into the searchbox. js files have been renamed and cleaned up. starting to understand this message sending system.

---
+++ content.js
@@ -0,0 +1,13 @@
+chrome.runtime.onMessage.addListener(function(req, sender, sendResponse) {
+    if ( req.type === "getSelection" ) {        
+        var sel = document.getSelection().toString();
+        if ( sel.trim().length > 0 ) {
+            sendResponse({
+                type: "selection", 
+                message: sel.trim()
+            });
+        } else {
+            sendResponse({})
+        }
+    }
+})
--- popup.js
+++ popup.js
@@ -1,27 +1,28 @@
-var butt   = document.getElementById('butt')
-  , uri    = "http://muhlenberg.worldcat.org/search?qt=wc_org_muhlenberg&ai=wclocal_muhlenberg&q="
-    ;
+chrome.tabs.query({ active: true }, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { type: 'getSelection' }, function(response) {
+        if ( response && response.type === 'selection' ) {  
+            document.getElementById('search').value = response.message;
+        }
+    })
+})
+
+document.getElementById('butt').onclick = function (e) {
+    e.preventDefault();
+    console.log('clickd')
+    search(document.getElementById('search').value);
+}
 
 window.addEventListener("keydown", function(e) {
     if ( e.keyCode === 13 ) {
-        search();
+        search(document.getElementById('search').value);
     }
 })
 
-butt.onclick = function (e) {
-    e.preventDefault();
-    search();
-}
+function search(terms) {
+    terms = terms.trim();
 
-function search() {
-
-    search = document.getElementById('search').value;
-    search = search.trim();
-
-    if ( search.length != 0 ) {
-        chrome.tabs.create({url: uri + encodeURI(search)});
-    } else {
-        return;
+    if ( terms.length > 0 ) {
+        var uri = 'http://muhlenberg.worldcat.org/search?qt=wc_org_muhlenberg&ai=wclocal_muhlenberg&q='
+        chrome.tabs.create({ url: uri + encodeURI(terms) });
     }
-
-};
+}
